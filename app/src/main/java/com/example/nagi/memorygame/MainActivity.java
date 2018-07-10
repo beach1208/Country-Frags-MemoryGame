a49c59e30b08007d27ff6b80764874da60f8275a
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-07-10 12:08:29 -0700

    Countdown timer added.

--- app/src/main/java/com/example/nagi/memorygame/MainActivity.java
+++ app/src/main/java/com/example/nagi/memorygame/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nagi.memorygame;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +19,7 @@ import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewp1,textViewp2;
+    TextView textViewp1,timer_View;
     ImageView questionImage11,questionImage12,questionImage13,questionImage14,
             questionImage21,questionImage22,questionImage23,questionImage24,
             questionImage31,questionImage32,questionImage33,questionImage34;
@@ -36,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     int cardNumber = 1;
 
     int turn = 1;
-    int playerPoints = 0, cpuPoints = 0;
+    int playerPoints = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +45,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewp1 = findViewById(R.id.tv_p1);
-        textViewp2 = findViewById(R.id.tv_p2);
+        timer_View = findViewById(R.id.timer_View);
+
+        //CountDown timer
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timer_View.setText(millisUntilFinished / 1000+ " seconds remaining");
+            }
+
+            public void onFinish() {
+                timer_View.setText("END!");
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder
+                        .setMessage("GAME OVER!\nP1: " + playerPoints)
+                        .setCancelable(false)
+                        .setPositiveButton("NEW", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        })
+                        .setNegativeButton("EXIT", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        }.start();
+
 
         questionImage11 = findViewById(R.id.questionImageView11);
         questionImage12 = findViewById(R.id.questionImageView12);
@@ -98,7 +133,6 @@ public class MainActivity extends AppCompatActivity {
         Collections.shuffle(Arrays.asList(cardArray));
 
         //Changing the color of the second player(inactive)
-        textViewp2.setTextColor(Color.GRAY);
 
         cardOnclick();
 
@@ -119,31 +153,50 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private boolean setEnabled(Boolean enable){
+        for (int i = 0; i < imageViewArrayList.size(); i++){
+            imageViewArrayList.get(i).setEnabled(enable);
+        }
+        return enable;
+    }
+
     private void doStuff(ImageView iv, int card){
-        if(cardArray[card] == 101){
-            iv.setImageResource(image101);
-        }else if(cardArray[card] == 102){
-            iv.setImageResource(image102);
-        }else if(cardArray[card] == 103){
-            iv.setImageResource(image103);
-        }else if(cardArray[card] == 104){
-            iv.setImageResource(image104);
-        }else if(cardArray[card] == 105){
-            iv.setImageResource(image105);
-        }else if(cardArray[card] == 106){
-            iv.setImageResource(image106);
-        }else if(cardArray[card] == 201){
-            iv.setImageResource(image201);
-        }else if(cardArray[card] == 202){
-            iv.setImageResource(image202);
-        }else if(cardArray[card] == 203){
-            iv.setImageResource(image203);
-        }else if(cardArray[card] == 204){
-            iv.setImageResource(image204);
-        }else if(cardArray[card] == 205){
-            iv.setImageResource(image205);
-        }else if(cardArray[card] == 206){
-            iv.setImageResource(image206);
+        switch (cardArray[card]){
+            case 101:
+                iv.setImageResource(image101);
+                break;
+            case 102:
+                iv.setImageResource(image102);
+                break;
+            case 103:
+                iv.setImageResource(image103);
+                break;
+            case 104:
+                iv.setImageResource(image104);
+                break;
+            case 105:
+                iv.setImageResource(image105);
+                break;
+            case 106:
+                iv.setImageResource(image106);
+                break;
+            case 201:
+                iv.setImageResource(image201);
+                break;
+            case 202:
+                iv.setImageResource(image202);
+                break;
+            case 203:
+                iv.setImageResource(image203);
+                break;
+            case 204:
+                iv.setImageResource(image204);
+                break;
+            case 205:
+                iv.setImageResource(image205);
+                break;
+            case 206:
+                iv.setImageResource(image206);
         }
 
         //check which image is selected and save it to temporary variable
@@ -165,20 +218,7 @@ public class MainActivity extends AppCompatActivity {
             clickedSecond = card;
         }
 
-        questionImage11.setEnabled(false);
-        questionImage12.setEnabled(false);
-        questionImage13.setEnabled(false);
-        questionImage14.setEnabled(false);
-
-        questionImage21.setEnabled(false);
-        questionImage22.setEnabled(false);
-        questionImage23.setEnabled(false);
-        questionImage24.setEnabled(false);
-
-        questionImage31.setEnabled(false);
-        questionImage32.setEnabled(false);
-        questionImage33.setEnabled(false);
-        questionImage34.setEnabled(false);
+        setEnabled(false);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -194,92 +234,113 @@ public class MainActivity extends AppCompatActivity {
     private void calculate(){
         //if images are eaual remove them
         if (firstCard == secondCard){
-            if (clickedFirst == 0){
-                questionImage11.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 1){
-                questionImage12.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 2){
-                questionImage13.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 3){
-                questionImage14.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 4){
-                questionImage21.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 5){
-                questionImage22.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 6){
-                questionImage23.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 7){
-                questionImage24.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 8){
-                questionImage31.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 9){
-                questionImage32.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 10){
-                questionImage33.setVisibility(View.INVISIBLE);
-            }else if(clickedFirst == 11){
-                questionImage34.setVisibility(View.INVISIBLE);
+            switch (clickedFirst){
+                case 0:
+                    questionImage11.setVisibility(View.INVISIBLE);
+                    break;
+                case 1:
+                    questionImage12.setVisibility(View.INVISIBLE);
+                    break;
+                case 2:
+                    questionImage13.setVisibility(View.INVISIBLE);
+                    break;
+                case 3:
+                    questionImage14.setVisibility(View.INVISIBLE);
+                    break;
+                case 4:
+                    questionImage21.setVisibility(View.INVISIBLE);
+                    break;
+                case 5:
+                    questionImage22.setVisibility(View.INVISIBLE);
+                    break;
+                case 6:
+                    questionImage23.setVisibility(View.INVISIBLE);
+                    break;
+                case 7:
+                    questionImage24.setVisibility(View.INVISIBLE);
+                    break;
+                case 8:
+                    questionImage31.setVisibility(View.INVISIBLE);
+                    break;
+                case 9:
+                    questionImage32.setVisibility(View.INVISIBLE);
+                    break;
+                case 10:
+                    questionImage33.setVisibility(View.INVISIBLE);
+                    break;
+                case 11:
+                    questionImage34.setVisibility(View.INVISIBLE);
+                    break;
+                default:
+                    break;
             }
 
-            if (clickedSecond == 0){
-                questionImage11.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 1){
-                questionImage12.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 2){
-                questionImage13.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 3){
-                questionImage14.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 4){
-                questionImage21.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 5){
-                questionImage22.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 6){
-                questionImage23.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 7){
-                questionImage24.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 8){
-                questionImage31.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 9){
-                questionImage32.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 10){
-                questionImage33.setVisibility(View.INVISIBLE);
-            }else if(clickedSecond == 11){
-                questionImage34.setVisibility(View.INVISIBLE);
+
+            switch (clickedSecond){
+                case 0:
+                    questionImage11.setVisibility(View.INVISIBLE);
+                    break;
+                case 1:
+                    questionImage12.setVisibility(View.INVISIBLE);
+                    break;
+                case 2:
+                    questionImage13.setVisibility(View.INVISIBLE);
+                    break;
+                case 3:
+                    questionImage14.setVisibility(View.INVISIBLE);
+                    break;
+                case 4:
+                    questionImage21.setVisibility(View.INVISIBLE);
+                    break;
+                case 5:
+                    questionImage22.setVisibility(View.INVISIBLE);
+                    break;
+                case 6:
+                    questionImage23.setVisibility(View.INVISIBLE);
+                    break;
+                case 7:
+                    questionImage24.setVisibility(View.INVISIBLE);
+                    break;
+                case 8:
+                    questionImage31.setVisibility(View.INVISIBLE);
+                    break;
+                case 9:
+                    questionImage32.setVisibility(View.INVISIBLE);
+                    break;
+                case 10:
+                    questionImage33.setVisibility(View.INVISIBLE);
+                    break;
+                case 11:
+                    questionImage34.setVisibility(View.INVISIBLE);
+                    break;
+                default:
+                    break;
             }
 
-            if (turn == 1){
+            if (turn == 1) {
                 playerPoints++;
-                textViewp1.setText("P1:" + playerPoints);
-            }else if (turn == 2){
-                cpuPoints++;
-                textViewp2.setText("P2: " + cpuPoints);
+                textViewp1.setText("Points:" + playerPoints);
             }
-        }else {
+//            }else if (turn == 2){
+//                cpuPoints++;
+//                textViewp2.setText("P2: " + cpuPoints);
+//            }
 
+        }else {
+            //setting Images to question Images
             setQuestionImages();
-            if (turn == 1){
-                turn = 2;
-                textViewp1.setTextColor(Color.GRAY);
-                textViewp2.setTextColor(Color.BLACK);
-            }else if(turn == 2){
-                turn = 1;
-                textViewp2.setTextColor(Color.GRAY);
-                textViewp1.setTextColor(Color.BLACK);
-            }
+
+//            if (turn == 1){
+//                turn = 2;
+//                textViewp1.setTextColor(Color.GRAY);
+//            }else if(turn == 2){
+//                turn = 1;
+//                textViewp1.setTextColor(Color.BLACK);
+//            }
         }
 
-
-        questionImage11.setEnabled(true);
-        questionImage12.setEnabled(true);
-        questionImage13.setEnabled(true);
-        questionImage14.setEnabled(true);
-        questionImage21.setEnabled(true);
-        questionImage22.setEnabled(true);
-        questionImage23.setEnabled(true);
-        questionImage24.setEnabled(true);
-        questionImage31.setEnabled(true);
-        questionImage32.setEnabled(true);
-        questionImage33.setEnabled(true);
-        questionImage34.setEnabled(true);
+        //set Enable method
+        setEnabled(true);
 
         //check if game is over
         checkEnd();
@@ -287,63 +348,67 @@ public class MainActivity extends AppCompatActivity {
 
     private void setQuestionImages(){
         for (int i = 0; i < imageViewArrayList.size(); i++){
-            imageViewArrayList.get(i).setImageResource(R.drawable.question_mark);
+            imageViewArrayList.get(i).setImageResource(R.drawable.world);
         }
     }
 
-    private void checkEnd(){
+
+    private void checkEnd() {
         if (
                 questionImage11.getVisibility() == View.INVISIBLE &&
-                questionImage12.getVisibility() == View.INVISIBLE &&
-                questionImage13.getVisibility() == View.INVISIBLE &&
-                questionImage14.getVisibility() == View.INVISIBLE &&
-                questionImage21.getVisibility() == View.INVISIBLE &&
-                questionImage22.getVisibility() == View.INVISIBLE &&
-                questionImage23.getVisibility() == View.INVISIBLE &&
-                questionImage24.getVisibility() == View.INVISIBLE &&
-                questionImage31.getVisibility() == View.INVISIBLE &&
-                questionImage32.getVisibility() == View.INVISIBLE &&
-                questionImage33.getVisibility() == View.INVISIBLE &&
-                questionImage34.getVisibility() == View.INVISIBLE){
+                        questionImage12.getVisibility() == View.INVISIBLE &&
+                        questionImage13.getVisibility() == View.INVISIBLE &&
+                        questionImage14.getVisibility() == View.INVISIBLE &&
+                        questionImage21.getVisibility() == View.INVISIBLE &&
+                        questionImage22.getVisibility() == View.INVISIBLE &&
+                        questionImage23.getVisibility() == View.INVISIBLE &&
+                        questionImage24.getVisibility() == View.INVISIBLE &&
+                        questionImage31.getVisibility() == View.INVISIBLE &&
+                        questionImage32.getVisibility() == View.INVISIBLE &&
+                        questionImage33.getVisibility() == View.INVISIBLE &&
+                        questionImage34.getVisibility() == View.INVISIBLE) {
+            {
 
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-            alertDialogBuilder
-                    .setMessage("GAME OVER!\nP1: " + playerPoints + "\nP2: " + cpuPoints)
-                    .setCancelable(false)
-                    .setPositiveButton("NEW", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                            startActivity(intent);
-                            finish();
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder
+                        .setMessage("GAME OVER!\nP1: " + playerPoints)
+                        .setCancelable(false)
+                        .setPositiveButton("NEW", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
 
-                        }
-                    })
-                    .setNegativeButton("EXIT", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            finish();
-                        }
-                    });
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.show();
+                            }
+                        })
+                        .setNegativeButton("EXIT", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
         }
     }
 
-    private void frontOfCardsResources(){
-        image101 = R.drawable.image101;
-        image102 = R.drawable.image102;
-        image103 = R.drawable.image103;
-        image104 = R.drawable.image104;
-        image105 = R.drawable.image105;
-        image106 = R.drawable.image106;
+        private void frontOfCardsResources () {
+            image101 = R.drawable.image101;
+            image102 = R.drawable.image102;
+            image103 = R.drawable.image103;
+            image104 = R.drawable.image104;
+            image105 = R.drawable.image105;
+            image106 = R.drawable.image106;
 
-        image201 = R.drawable.image201;
-        image202 = R.drawable.image202;
-        image203 = R.drawable.image203;
-        image204 = R.drawable.image204;
-        image205 = R.drawable.image205;
-        image206 = R.drawable.image206;
+            image201 = R.drawable.image201;
+            image202 = R.drawable.image202;
+            image203 = R.drawable.image203;
+            image204 = R.drawable.image204;
+            image205 = R.drawable.image205;
+            image206 = R.drawable.image206;
 
-    }
+        }
+
 }
